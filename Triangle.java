7f81960029fc6a18731a1231f33f636f42302dfe
Nickolas Natali <nicka.natali@calbaptist.edu>
2016-10-03 15:09:19 -0700

    Add files via upload

--- Triangle.java
+++ Triangle.java
@@ -1,8 +1,7 @@
-
-
 /**
- * Created by NickNatali on 9/12/16.
+ * Created by NickNatali on 9/28/16.
  */
+
 public class Triangle {
 
     private double sideA;
@@ -17,21 +16,29 @@ public class Triangle {
         sideC = c;
     }
 
+    public static boolean notATriangle(double a, double b, double c)
+    {
+        //This is notATriangle when these expressions are NOT true.
+        return !((c + a > b) && (b + a > c) && (b + c > a));
+    }
+
     public boolean isEquilateral() {
+        //The triangle is an equilateral when these statements ARE true.
         return (sideA == sideB) && (sideA == sideC);
     }
 
     public boolean isIsosceles() {
+        //The triangle is an isosceles when these statements ARE true.
         return(sideA == sideB) || (sideB == sideC) || (sideA == sideB);
     }
 
     public boolean isScalene() {
+        //The triangle is an scalene when these statements ARE true.
         return(sideA != sideB) && (sideB != sideC) && (sideA != sideC);
     }
 
-    public boolean isTriangle() {
-        return (sideA > 0) && (sideB > 0) && (sideC > 0) && (sideA + sideB > sideC) && (sideB + sideC > sideA) && (sideA + sideC > sideB);
-    }
+
+
 
 
 }
